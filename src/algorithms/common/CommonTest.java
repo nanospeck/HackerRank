75ed69e0c37f82651d4472f984f445b2953c0737
Akhil Anil <akhil.karun@gmail.com>
2016-03-06 23:16:36 +0530

    Mavenized the project and added a rought tester file

---
+++ src/algorithms/common/CommonTest.java
@@ -0,0 +1,16 @@
+package algorithms.common;
+
+public class CommonTest {
+	
+	public static void main(String[] args){
+		
+		testIsaWholeNumber();
+	}
+
+	private static void testIsaWholeNumber() {
+		// TODO Auto-generated method stub
+		System.out.println(Common.isWholeNumber(10.4));
+		System.out.println(Common.isWholeNumber(10));
+	}
+
+}
